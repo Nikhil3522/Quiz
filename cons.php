85983c7729ed0e11b8aea4464ef98c6496412fe9
Nikhil3522 <nikhiljha3522@gmail.com>
2024-12-25 12:39:53 +0530

    mysql database connection

---
+++ cons.php
@@ -0,0 +1,14 @@
+<?php
+@session_start();
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$conn->set_charset("utf8");
+
+?>

02ed3d1a4343b00e8113ca6f07f7ec9723dbbdf2
Nikhil3522 <nikhiljha3522@gmail.com>
2025-03-07 12:21:36 +0530

    pricing page

---
+++ pricing-plan.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from quizio-pwa-html-app.vercel.app/sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 Dec 2024 12:49:35 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="shortcut icon"
+      href="assets/images/letter-w.png"
+      type="image/x-icon"
+    />
+    <link rel="stylesheet" href="assets/css/swiper.min.css" />
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <link rel="manifest" href="manifest.json" />
+    <title>Whiteboard</title>
+  <link href="style.css" rel="stylesheet">
+  <link href="pricing-plan.css" rel="stylesheet"></head>
+
+  <body class="">
+    <div
+      class="container min-h-dvh relative overflow-hidden py-8 px-6 dark:text-white dark:bg-color1"
+    >
+      <!-- Absolute Items Start -->
+      <img
+        src="assets/images/header-bg-2.png"
+        alt=""
+        class="absolute top-0 left-0 right-0 -mt-6"
+      />
+      <div
+        class="absolute top-0 left-0 bg-p3 blur-[145px] h-[174px] w-[149px]"
+      ></div>
+      <div
+        class="absolute top-40 right-0 bg-[#0ABAC9] blur-[150px] h-[174px] w-[91px]"
+      ></div>
+      <div
+        class="absolute top-80 right-40 bg-p2 blur-[235px] h-[205px] w-[176px]"
+      ></div>
+      <div
+        class="absolute bottom-0 right-0 bg-p3 blur-[220px] h-[174px] w-[149px]"
+      ></div>
+      <!-- Absolute Items End -->
+      <!-- Page Title Start -->
+      <div class="flex justify-start items-center gap-4 relative z-10">
+        <a
+          href="home.html"
+          class="bg-white p-2 rounded-full flex justify-center items-center text-xl dark:bg-color10"
+        >
+          <i class="ph ph-caret-left"></i>
+        </a>
+        <h2 class="text-2xl font-semibold text-white">Choose Your Plan</h2>
+      </div>
+      <!-- Page Title End -->
+
+      <div class="pricing-wrap section-pb-60" style="margin-bottom: 80px; margin-top: 80px;">
+        <div class="container d-flex">
+            <div class="row row-cols-md-3 row-cols-1" id="pricing_plan_card_container" style="margin: auto; row-gap: 40px;">
+                
+                <div class="pricing-plan-card">
+                    <div>
+                        <h2 class="text-center pt-3" style="color: white;">Daily</h2>
+                    </div>
+                    <h1 style="display: flex; justify-content: center;"><img src="assets/images/AFN-currency.png" style="width: 30px; height: 55px; margin-top: 15px;"><span>10</span></h1>
+                    <div style="min-height: 300px; background: whitesmoke; padding-top: 45px; padding-bottom: 10px; display: flex; flex-direction: column; border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                        <ul class="pricing_plan_ul">
+                            <li>Pay per day </li>
+                            <li>No commitment </li>
+                            <li>Ideal for trials </li>
+                        </ul>
+                        <button onclick="window.location.href='home-test.html'">Choose plan</button>
+                        <p style="font-size: 9px; line-height: 12px; text-align: center;">Cancel anytime, 24/7 customer support </p>
+                    </div>
+                </div>
+
+                <div class="pricing-plan-card">
+                    <div style="min-height: 140px;">
+                        <h2 class="text-center pt-3" style="color: white;">Monthly</h2>
+                    </div>
+                    <div class="ribbon left">Best value deal </div>
+                    <h1 style="display: flex; justify-content: center;"><img src="assets/images/AFN-currency.png" style="width: 30px; height: 55px; margin-top: 15px;"><span>90</span></h1>
+                    <div style="min-height: 320px; background: whitesmoke; padding-top: 45px; padding-bottom: 10px; display: flex; flex-direction: column; border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                        <ul class="pricing_plan_ul">
+                            <li>Best value deal </li>
+                            <li>Unlimited monthly access </li>
+                            <li>Hassle-free subscription </li>
+                        </ul>
+                        <button onclick="window.location.href='home-test.html'">Choose plan</button>
+                        <p style="font-size: 9px; line-height: 12px; text-align: center;">Cancel anytime, 24/7 customer support </p>
+                    </div>
+                </div>
+
+
+                <div class="pricing-plan-card">
+                    <div>
+                        <h2 class="text-center pt-3" style="color: white;">Weekly</h2>
+                    </div>
+                    <h1 style="display: flex; justify-content: center;"><img src="assets/images/AFN-currency.png" style="width: 30px; height: 55px; margin-top: 15px;"><span>20</span></h1>
+                    <div style="min-height: 300px; background: whitesmoke; padding-top: 45px; padding-bottom: 10px; display: flex; flex-direction: column; border-bottom-left-radius: 20px; border-bottom-right-radius: 20px;">
+                        <ul class="pricing_plan_ul">
+                            <li>Affordable short-term option </li>
+                            <li>Save on daily rates </li>
+                            <li>Best for quick use </li>
+                        </ul>
+                        <button onclick="window.location.href='home-test.html'">Choose plan</button>
+                        <p style="font-size: 9px; line-height: 12px; text-align: center;">Cancel anytime, 24/7 customer support </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Javascript Dependencies -->
+    <script src="assets/js/main.js"></script>
+  <script defer src="index.js"></script></body>
+
+<!-- Mirrored from quizio-pwa-html-app.vercel.app/sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 Dec 2024 12:49:40 GMT -->
+</html>

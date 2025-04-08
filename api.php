2d944e47ddcba2e41f5b45917cf24f5049020aa4
Nikhil3522 <nikhiljha3522@gmail.com>
2025-04-08 12:33:22 +0530

    add user api function

--- api.php
+++ api.php
@@ -3,10 +3,50 @@
 // error_reporting(E_ALL);
 
 include('cons.php');
-$function_name = $_GET['function_name'];
+$function_name = isset($_GET['function_name']) ? $_GET['function_name'] : $_GET['func'];
 $user_id = 1;
 
 switch ($function_name) {
+    case 'ADD_USER':
+
+        $msisdn = $_POST['msisdn'];
+
+        $query = "SELECT * from users where msisdn = '$msisdn'";
+        $result = $conn->query($query);
+
+        if($result->num_rows > 0){
+
+            $user_data = $result->fetch_assoc();
+            $_SESSION['user_id'] = $user_data['id'];
+
+        }else{
+            $query = "INSERT INTO users (msisdn) VALUES ('$msisdn')";
+            $insert_data = $conn->query($query);
+
+            if ($insert_data) {
+                // Get the auto-incremented ID of the newly inserted row
+                $new_id = $conn->insert_id;
+                $_SESSION["user_id"] = $new_id;
+                setcookie(
+                    'user_id', 
+                    $new_id, 
+                    time() + (60*60),  // Cookie expiry
+                    '/',  // Path
+                    '',   // Domain
+                    true, // Secure (only send over HTTPS)
+                    true // HttpOnly (restricts JavaScript access)
+                );
+                
+            } else {
+                // Handle the error if the query fails
+                echo "Error in query: " . $conn->error;
+            }
+        }
+
+        // die("DONE");
+        
+        header('Location: index.php');
+        break;
     case 'load_questions':
 
         if($_GET['quiz_level']){

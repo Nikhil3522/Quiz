53ae170ee34a39c934b413830b1c298dd41496d6
Nikhil3522 <nikhiljha3522@gmail.com>
2025-04-22 17:28:30 +0530

    set user_id null on logout

--- logout.php
+++ logout.php
@@ -1,10 +1,19 @@
 <?php
-setcookie(
+  // unset the session
+  session_unset();
+  setcookie(
     'mobilenumber', 
     null, 
     time() + (60*60),  // Cookie expiry
     '/',  // Path
     ''   // Domain
   );
-    Header('Location: sign-in.html');
+  setcookie(
+    'user_id', 
+    null, 
+    time() + (60*60),  // Cookie expiry
+    '/',  // Path
+    ''   // Domain
+  );
+    Header('Location: sign-in.php');
 ?>
